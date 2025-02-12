c2bd0c525d5bafaf27e586d41a699edea3d63e6f
VibolSovichea <sovicheavibol9@gmail.com>
2025-02-12 19:02:04 +0700

    Testing features

---
+++ src/app/signin/page.tsx
@@ -0,0 +1,9 @@
+import Base from "@/components/shared/base-layout"
+
+export default function SignInPage () {
+  return (
+    <Base>
+      <div>Sign IN page</div>
+    </Base>
+  )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,9 +9,34 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: "#3BDA34",
+        secondary: "#FEFFF4",
+        muted: "#D9D9D9",
+        dark: "#1A1A1A",
+        light: "#F2F2F2",
       },
+      spacing: {
+				'xs': '4px',
+				'sm': '8px',
+				'md': '16px',
+				'lg': '24px',
+				'xl': '32px',
+				'2xl': '40px',
+				'3xl': '48px',
+
+        "small": "0.5rem",
+				"quarter": "0.75rem",
+        "medium": "1rem",
+				"half": "1.5rem",
+        "large": "2rem",
+				"base": "3rem",
+			},
+      fontSize: {
+        "title" : "36px",
+        "subtitle" : "32px",
+        "label" : "16px",
+        "body" : "14px",
+      }
     },
   },
   plugins: [],
