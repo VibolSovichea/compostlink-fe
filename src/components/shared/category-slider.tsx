739e41162cfeebfed32fac041dd896f643ed615e
VibolSovichea <sovicheavibol9@gmail.com>
2025-03-21 23:33:38 +0700

    Enhance

--- src/components/shared/category-slider.tsx
+++ src/components/shared/category-slider.tsx
@@ -4,7 +4,7 @@ interface CategorySliderProps {
 
 const CategorySlider = ({ categories }: CategorySliderProps) => {
   return (
-    <div className="flex gap-2 overflow-x-auto">
+    <div className="flex gap-2 overflow-x-auto no-scrollbar">
       {categories.map((category) => (
         <div key={category} className="bg-primary rounded-xl px-4 py-2">
           {category}
